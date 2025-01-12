7848fff7c77e02570c229f23af14a6dcc6b89f3a
pedrohpiri <pedropiri.h@gmail.com>
2025-01-12 02:58:09 -0300

    atualizaçoes

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import path from 'path'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Substituindo __dirname para ESM
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'), // Mantendo __dirname
+      '@': path.resolve(__dirname, './src'), // Alias para facilitar imports
     },
   },
-})
+});
